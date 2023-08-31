44764883051db6b73f52143f59f0bc09e6aab0e5
evaldo <evaldo.junior@etec.sp.gov.br>
2023-08-31 13:18:47 -0300

    .

--- lib/sembast_database.dart
+++ lib/sembast_database.dart
@@ -3,15 +3,15 @@ import 'package:sembast/sembast_io.dart';
 
 class SembastDatabase {
   
-String dbPath = 'tarefas.db';
-DatabaseFactory dbFactory = databaseFactoryIo;
-late Database db;
+  String dbPath = 'tarefas.db';
+  DatabaseFactory dbFactory = databaseFactoryIo;
+  late Database db;
 
-getInstance() async {
-  if (db==null) {
-    db = await dbFactory.openDatabase(dbPath);
+  getInstance() async {
+    if (db==null) {
+      db = await dbFactory.openDatabase(dbPath);
+    }
+    return db;
   }
-  return db;
-}
 
 }
