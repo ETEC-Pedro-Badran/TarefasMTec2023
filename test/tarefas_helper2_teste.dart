7f0f34eded0d033da74738e1c07f18bddebc636c
evaldo <evaldo.junior@etec.sp.gov.br>
2023-09-14 13:24:13 -0300

    Lista salvando.

--- test/tarefas_helper2_teste.dart
+++ test/tarefas_helper2_teste.dart
@@ -6,13 +6,12 @@ import 'package:tarefas/tarefas_helper_impl.dart';
 void main() {
   LiveTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('tarefas helper ...', (tester) async {
+  testWidgets('tarefas helper:salvar ...', (tester) async {
 
     var tarefa = Tarefa(descricao: "Tarefa de Teste", prazo: DateTime.now().add(Duration(days: 30)));
 
     TarefasHelper helper = TarefasHelperImpl();
     int id = await helper.salvar(tarefa);
     print("Tarefa incluída com o id $id");
-
   });
 }
